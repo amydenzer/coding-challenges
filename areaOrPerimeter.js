efaa7df0763385f14a3c9b858303a5ec1f939a67
amydenzer <amydenzer@gmail.com>
2023-08-24 23:02:55 -0500

    area or perimeter

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -8,7 +8,6 @@
 // 3, 3 --> 9
 // Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 
-
 // first attempt
 
 const areaOrPerimeter = function(l , w) {

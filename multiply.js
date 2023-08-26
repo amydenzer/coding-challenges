3157ec22f38f54b2555acb4b25108cc0970d2cd2
amydenzer <amydenzer@gmail.com>
2023-08-26 02:02:52 -0500

    Multiply

---
+++ multiply.js
@@ -0,0 +1,22 @@
+// This code does not execute properly. Try to figure out why.
+
+
+//First attempt
+multiply = function (a, b) {
+    return a * b;
+  }
+
+
+
+//  multiply = (a, b) => a * b;
+
+
+// multiply = (a, b) => a * b;
+
+
+// function multiply(a, b){
+//     if (undefined == a || undefined == b
+//         || isNaN(a) || isNaN(b))
+//         throw new Error('Both arguments should be numbers')
+//       return a * b
+//     }

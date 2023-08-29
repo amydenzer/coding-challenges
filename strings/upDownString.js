26d59904e6b4f3a9aa98be2de9e3a12584198ffc
amydenzer <amydenzer@gmail.com>
2023-08-29 18:33:33 -0500

    commit up and down, the string grows 8kyu string kata

---
+++ strings/upDownString.js
@@ -0,0 +1,38 @@
+// Many programming languages provide the functionality of converting a string to uppercase or lowercase. For example, upcase/downcase in Ruby, upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript. Typically, these methods won't change the size of the string.
+
+// For example, in Ruby, str.upcase.downcase.size == str.size is true for most cases.
+
+// However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+
+// For example, in Ruby, That means str.upcase.downcase.size > str.size
+
+// You should just set the value of STRANGE_STRING to meet the previous criteria.
+
+// Note: Meta programming is not allowed in this kata. So, the size of your solution is limited.
+
+//My Solution
+const STRANGE_STRING = "ß"
+
+
+//Other Solutions
+const STRANGE_STRING = "oßo"
+
+const STRANGE_STRING = "aß"
+
+const STRANGE_STRING ="\u0130"
+
+const STRANGE_STRING = 'ῗ'
+
+
+//Test Case
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("Checking string length", function() {
+    const upDownLen = STRANGE_STRING.toUpperCase().toLowerCase().length;
+    const oriLen = STRANGE_STRING.length;
+
+    it("String length should grow", function() {
+        assert.isAbove(upDownLen, oriLen, `No grow, actual length: ${oriLen} ⇒ ${upDownLen}`);
+    });
+});

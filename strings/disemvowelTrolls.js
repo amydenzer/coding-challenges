6f040e5f477f90988944cbcb3473c73d54f590f6
amydenzer <amydenzer@gmail.com>
2023-09-14 21:38:41 -0500

    push code to 7kyu string kata

--- strings/disemvowelTrolls.js
+++ strings/disemvowelTrolls.js
@@ -40,6 +40,22 @@ function disemvowel(str) {
       .join('');
   }
 
+//others
+function getCount(str) {
+    return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+   }
+
+
+//others
+function getCount(str) {
+    var vowelsCount = 0;
+    str.split("").forEach(function(x){
+      if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+        vowelsCount += 1;
+      }
+    });  
+    return vowelsCount;
+  }
 
 
 //Test case

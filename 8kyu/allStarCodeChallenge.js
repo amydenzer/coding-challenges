8935921a6c99bc0e944dc724c24bc3fe88278555
amydenzer <amydenzer@gmail.com>
2024-01-26 23:42:12 -0600

    push 8kyu kata all star challenge

---
+++ 8kyu/allStarCodeChallenge.js
@@ -0,0 +1,24 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+
+//Solution
+function strCount(str, letter){  
+    return str.split(letter).length-1
+  }
+
+//Tests
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(strCount('Hello', 'o'), 1);
+    assert.strictEqual(strCount('Hello', 'l'), 2);
+    assert.strictEqual(strCount('',      'z'), 0);
+  });
+});
+

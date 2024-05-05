f9b1be22e12327402b04f5bec97055f37a04d943
amydenzer <amydenzer@gmail.com>
2024-05-05 13:31:58 -0500

    push sunday codewar 8kyu

--- 8kyu/switchItUp.js
+++ 8kyu/switchItUp.js
@@ -8,38 +8,18 @@
 
 // Solution
 function switchItUp(number) {
-    switch (num) {
-        case 1:
-            console.log('one');
-            break;
-        case 2:
-            console.log('two');
-            break;
-        case 3:
-            console.log('three');
-            break;
-        case 4:
-            console.log('four');
-            break;
-        case 5:
-            console.log('five');
-            break;                   
-        case 6:
-            console.log('six');
-            break;
-        case 7:
-            console.log('seven');
-            break;
-        case 8:
-            console.log('eight');
-            break;
-        case 9:
-            console.log('nine');
-            break;    
-        case 10:
-            console.log('ten');
-            break;                            
-      default:
-        console.log(`Sorry, this is not a number`)
+    switch(number) {
+        case 0: return "Zero";
+        case 1: return "One";
+        case 2: return "Two";
+        case 3: return "Three";
+        case 4: return "Four";
+        case 5: return "Five";
+        case 6: return "Six";
+        case 7: return "Seven";
+        case 8: return "Eight";
+        case 9: return "Nine";
+        default: return "Invalid number"; 
+    }
 
     };

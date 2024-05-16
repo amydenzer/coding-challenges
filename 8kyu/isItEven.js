3867f84ab6b607238cc2fee3b0dc86128b196bbc
amydenzer <amydenzer@gmail.com>
2024-05-15 23:26:43 -0500

    commit 8kyu

---
+++ 8kyu/isItEven.js
@@ -0,0 +1,33 @@
+// DESCRIPTION:
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+
+// Solution
+function testEven(n) {
+    if (!Number.isInteger(n)) {
+        return false;
+    }
+    return n % 2 === 0;
+}
+// Test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("visible_tests", () => {
+    assert.strictEqual(testEven(0), true, "testEven for 0");
+    assert.strictEqual(testEven(0.5), false, "testEven for 0.5");
+    assert.strictEqual(testEven(1), false, "testEven for 1");
+    assert.strictEqual(testEven(2), true, "testEven for 2");
+    assert.strictEqual(testEven(-4), true, "testEven for 2");
+  });
+});

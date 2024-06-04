d9d8cb8147b317399d295c55ade8752489792021
amydenzer <amydenzer@gmail.com>
2024-06-03 21:38:03 -0500

    expressions matter kata

--- 8kyu/expressionsMatter.js
+++ 8kyu/expressionsMatter.js
@@ -1,4 +1,3 @@
-
 // Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
 // In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
 // Example
@@ -29,6 +28,17 @@ function expressionMatter(a, b, c) {
     ];
     return Math.max(...results);
   }
+// Another Solution
+function expressionMatter(a, b, c) {
+    return Math.max(
+      a + b + c,
+      a * b * c,
+      a * (b + c),
+      (a + b) * c,
+      a + b * c,
+      a * b + c,
+    );
+  }  
 
 // Test
 const chai = require("chai");

42a4e39256aa959a2049090b97a5689a71533b56
amydenzer <amydenzer@gmail.com>
2024-06-07 12:00:00 -0500

    sort and star

---
+++ 8kyu/sortAndStart.js
@@ -0,0 +1,28 @@
+// DESCRIPTION:
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+// Solution
+
+function twoSort(s) {
+    s.sort();
+    let firstStr = s[0];
+    return firstStr.split('').join('***');
+    }
+    
+// Another Solution
+    twoSort = s => s.sort()[0].split('').join('***')
+// Test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
+    assert.strictEqual(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e'); 
+  });
+});

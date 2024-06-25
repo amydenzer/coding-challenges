a9124c5f45f71ae3b6a4e63735dcd468aec9e464
amydenzer <amydenzer@gmail.com>
2024-06-25 09:08:11 -0500

    push 6kyu solution to Create Phone Number

---
+++ 6kyu/createPhoneNumber.js
@@ -0,0 +1,50 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+
+// Solution
+function createPhoneNumber(numbers) {
+    let firstpart = ""
+    let secondpart = ""
+    let thirdpart = ""
+    for (var i = 0; i < numbers.length; i++) {
+        if (i < 3) {
+            firstpart += numbers[i].toString()
+        } else if (i >= 3 && i < 6) {
+            secondpart += numbers[i].toString()
+        } else if (i >= 6) {
+            thirdpart += numbers[i].toString()
+        }
+    }
+    return `(${firstpart}) ${secondpart}-${thirdpart}`
+}
+
+// Solution 2
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+
+// Test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Create Phone Number", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+  });
+});
